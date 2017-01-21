f825e39dc127472d166303e726c48ff2269e01cd
Ken Sodetz <kensodetz@gmail.com>
2017-01-21 17:24:33 -0500

    added working dropdown list

--- app/src/main/java/edu/sodetzpurdue/goals_trak/addGoalActivity.java
+++ app/src/main/java/edu/sodetzpurdue/goals_trak/addGoalActivity.java
@@ -2,6 +2,8 @@ package edu.sodetzpurdue.goals_trak;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class AddGoalActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class AddGoalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_goal);
+        Spinner dropdown = (Spinner)findViewById(R.id.spinner);
+        String[] dropdownList = {"Hours", "Days", "Weeks", "Months", "Dollars", "Repititions", "Other"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, dropdownList);
+        dropdown.setAdapter(adapter);
     }
 }
