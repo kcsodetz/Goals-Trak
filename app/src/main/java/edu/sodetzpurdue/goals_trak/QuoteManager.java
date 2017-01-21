5cdba982f11566df8362589c6dedc25a7803c346
Ken Sodetz <kensodetz@gmail.com>
2017-01-21 16:01:05 -0500

    better stylized the quotation and fixed the title

--- app/src/main/java/edu/sodetzpurdue/goals_trak/QuoteManager.java
+++ app/src/main/java/edu/sodetzpurdue/goals_trak/QuoteManager.java
@@ -8,10 +8,10 @@ import java.util.Random;
 
 public class QuoteManager {
     public String[] quotes =
-            {"\"Fall down 7 times, stand up 8.\" Japanese Proverb",
-            "\"When you're going through hell, keep going.\" Winston Churchill",
-            "\"It always seems impossible until it's done.\" Nelson Mandela",
-            "\"It's not about where your starting point is, but your end goal and the journey that will get you there.\" Unknown"};
+            {"\"Fall down 7 times, stand up 8.\" \n-Japanese Proverb",
+            "\"When you're going through hell, keep going.\" \n-Winston Churchill",
+            "\"It always seems impossible until it's done.\" \n-Nelson Mandela",
+            "\"It's not about where your starting point is, but your end goal and the journey that will get you there.\" \n-Unknown"};
     public QuoteManager(){
 
     }
