ee70cb6df0764148d773be17ce4c5139f8836ebf
Ken Sodetz <kensodetz@gmail.com>
2017-01-21 16:27:25 -0500

    added ability to change between add goal and create new goal

--- app/src/main/java/edu/sodetzpurdue/goals_trak/CreateNewGoal.java
+++ app/src/main/java/edu/sodetzpurdue/goals_trak/CreateNewGoal.java
@@ -29,10 +29,14 @@ public class CreateNewGoal extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                changeView(view);
             }
         });
+    }
+
+    public void changeView(View view){
+        Intent intent = new Intent(this, AddGoalActivity.class);
+        startActivity(intent);
     }
 
 }
