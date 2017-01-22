2b947229ac2e3d83feb71efc37413f96e3aa9a3d
Ken Sodetz <kensodetz@gmail.com>
2017-01-22 09:12:23 -0500

    added some more quotes

--- app/src/main/java/edu/sodetzpurdue/goals_trak/QuoteManager.java
+++ app/src/main/java/edu/sodetzpurdue/goals_trak/QuoteManager.java
@@ -11,7 +11,9 @@ public class QuoteManager {
             {"\"Fall down 7 times, stand up 8.\" \n\t-Japanese Proverb",
             "\"When you're going through hell, keep going.\" \n\t-Winston Churchill",
             "\"It always seems impossible until it's done.\" \n\t-Nelson Mandela",
-            "\"It's not about where your starting point is, but your end goal and the journey that will get you there.\" \n\t-Unknown"};
+            "\"It's not about where your starting point is, but your end goal and the journey that will get you there.\" \n\t-Unknown",
+            "\"It's not who you think you are that holds you back it's who you think you're not.\"\n\t-Unknown",
+            "\"The first step to accepting yourself is to stop comparing yourself to others.\"\n\t-Joe Duncan"};
     public QuoteManager(){
 
     }
