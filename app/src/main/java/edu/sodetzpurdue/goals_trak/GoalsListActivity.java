29bcb88400125c1193933cbcb354ace84ff595bd
Ken Sodetz <kensodetz@gmail.com>
2017-01-22 08:39:45 -0500

    can now look at previously set goals

--- app/src/main/java/edu/sodetzpurdue/goals_trak/GoalsListActivity.java
+++ app/src/main/java/edu/sodetzpurdue/goals_trak/GoalsListActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -18,16 +19,52 @@ public class GoalsListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_goals_list);
         setTitle("Goals in Progress");
         listView = (ListView)findViewById(R.id.listView);
-
-        ArrayAdapter<GoalsManager> arrayAdapter = new ArrayAdapter<GoalsManager>(
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapter, View view, int position, long arg){
+                //Intent intent = new Intent(this, DisplayGoalActivity.class);
+                GoalsManager goalsmanager = (GoalsManager) adapter.getAdapter().getItem(position);
+                changeView(view, goalsmanager);
+            }
+        });
+        ArrayAdapter<GoalsManager> arrayAdapter = new ArrayAdapter<>(
                 this, android.R.layout.simple_list_item_1, arrayList
         );
         listView.setAdapter(arrayAdapter);
     }
 
-    public void changeView(View view){
-        Intent intent = new Intent(this, AddGoalActivity.class);
-        startActivity(intent);
+    public void changeView(View view, GoalsManager goalsManager){
+        Intent intent = new Intent(this, DisplayGoalActivity.class);
+        intent.putExtra("key", goalsManager);
+        startActivityForResult(intent, 12345);
     }
+
+    public View getViewByPosition(int pos, ListView listView) {
+        final int firstListItemPosition = listView.getFirstVisiblePosition();
+        final int lastListItemPosition = firstListItemPosition + listView.getChildCount() - 1;
+
+        if (pos < firstListItemPosition || pos > lastListItemPosition ) {
+            return listView.getAdapter().getView(pos, null, listView);
+        } else {
+            final int childIndex = pos - firstListItemPosition;
+            return listView.getChildAt(childIndex);
+        }
+    }
+
+   /*public void changeActivity(View view, ){
+        Intent intent = new Intent(this, DisplayGoalActivity.class);
+        GoalsManager goalsmanager =
+        intent.putExtra("key", goalsmanager);
+        startActivityForResult(intent, 12345);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent){
+        GoalsManager goalsmanager = new GoalsManager(goalName, amount, frequencySpinner, dayWeekMonthSpinner, hour_x, minute_x, ampm);
+        if (requestCode == 12345){
+            if (resultCode == RESULT_OK){
+                GoalsManager goalsManager = (GoalsManager)intent.getSerializableExtra("key");
+            }
+        }
+    }*/
 
 }
