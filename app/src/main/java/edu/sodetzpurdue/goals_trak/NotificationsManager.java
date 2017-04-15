508a9d9ce60641a45af2a82743523e3c3b78b254
Ken Sodetz <kensodetz@gmail.com>
2017-04-15 01:48:12 -0400

    Added code to the NotificationsManager.java class, beginning the framework for push notifications

--- app/src/main/java/edu/sodetzpurdue/goals_trak/NotificationsManager.java
+++ app/src/main/java/edu/sodetzpurdue/goals_trak/NotificationsManager.java
@@ -1,12 +1,59 @@
 package edu.sodetzpurdue.goals_trak;
 
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+
+import static android.content.Context.NOTIFICATION_SERVICE;
+
 /**
  * Created by Ken Sodetz on 4/14/2017.
  */
 
 public class NotificationsManager {
-    //TODO finish constructor
-    public NotificationsManager(){}
+
+    private int hour, minute;
+    private String ampm, frequency;
+    public NotificationsManager(int hour, int minute, String ampm, String frequency){
+        this.hour = hour;
+        this.minute = minute;
+        this.ampm = ampm;
+        this.frequency = frequency;
+    }
+
+    private Context context;
+    private NotificationCompat.Builder builder =
+            new NotificationCompat.Builder(context).setSmallIcon(R.drawable.ic_add_black_24dp).setContentTitle("Test Notification").setContentText("This is a comprehensive test");
+
+    private Intent resultIntent = new Intent(context, MainActivity.class);
+
+    // Because clicking the notification opens a new ("special") activity, there's
+    // no need to create an artificial back stack.
+    private PendingIntent resultPendingIntent =
+            PendingIntent.getActivity(
+                    context,
+                    0,
+                    resultIntent,
+                    PendingIntent.FLAG_UPDATE_CURRENT
+            );
+
+
+
+
+
+    public void pushNofitication(){
+        // Sets an ID for the notification
+        int mNotificationId = 001;
+// Gets an instance of the NotificationManager service
+        /*NotificationManager mNotifyMgr =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);*/
+// Builds the notification and issues it.
+        //mNotifyMgr.notify(mNotificationId, builder.build());
+    }
+
 
 
 }
